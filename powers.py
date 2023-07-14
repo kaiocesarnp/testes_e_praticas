a49b88edb6493867cfbda11e0b07790db0025e22
Kaio César <94173283+kaiocesarnp@users.noreply.github.com>
2023-07-14 12:32:29 -0300

    Update powers.py

--- powers.py
+++ powers.py
@@ -10,17 +10,20 @@ powers_sorted = sorted(powers)
 
 print("Poderes em ordem crescente:", powers_sorted)
 
+# Explicação linha à linha:
 
-num_powers = 4: Define a variável num_powers com o valor 4, que representa o número de valores que serão inseridos para os poderes.
+# print("Defina quatro valores de poder:"): Exibe esta mensagem no começo do código, instruindo o jogador ao que deve ser feito
 
-powers = []: Inicializa uma lista vazia chamada powers que será usada para armazenar os valores dos poderes.
+# num_powers = 4: Define a variável num_powers com o valor 4, que representa o número de valores que serão inseridos para os poderes.
 
-for i in range(num_powers):: Inicia um loop que irá iterar 4 vezes, uma para cada valor de poder que o usuário precisa inserir.
+# powers = []: Inicializa uma lista vazia chamada powers que será usada para armazenar os valores dos poderes.
 
-power = int(input("Digite um valor para power: ")): Solicita ao usuário que digite um valor para o poder e o converte para um número inteiro usando int(). O valor digitado é armazenado na variável power.
+# for i in range(num_powers):: Inicia um loop que irá iterar 4 vezes, uma para cada valor de poder que o usuário precisa inserir.
 
-powers.append(power): Adiciona o valor do poder à lista powers usando o método append(), para que seja armazenado para uso posterior.
+# power = int(input("Digite um valor para power: ")): Solicita ao usuário que digite um valor para o poder e o converte para um número inteiro usando int(). O valor digitado é armazenado na variável power.
 
-powers_sorted = sorted(powers): A função sorted() é usada para ordenar os valores na lista powers em ordem crescente, e o resultado é armazenado na lista powers_sorted.
+# powers.append(power): Adiciona o valor do poder à lista powers usando o método append(), para que seja armazenado para uso posterior.
 
-print("Poderes em ordem crescente:", powers_sorted): Imprime a frase "Poderes em ordem crescente:" seguida pelos valores dos poderes ordenados presentes na lista powers_sorted
+# powers_sorted = sorted(powers): A função sorted() é usada para ordenar os valores na lista powers em ordem crescente, e o resultado é armazenado na lista powers_sorted.
+
+# print("Poderes em ordem crescente:", powers_sorted): Imprime a frase "Poderes em ordem crescente:" seguida pelos valores dos poderes ordenados presentes na lista powers_sorted
